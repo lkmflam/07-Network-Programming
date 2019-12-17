c3e0518d7c94417daee7f8c54ef22ca297f8c6b4
lkmflam <53613483+lkmflam@users.noreply.github.com>
2019-12-17 14:17:26 -0600

    17 DEC - SOCKET UNDERSTANDING
    Best Explanation/ Exercise for Socket Understanding Socket Programming.

---
+++ 09_NetworkingExtended/Practice/02_Network/ChallengeLabs/Socket Challenge Lab #2 Server.py
@@ -0,0 +1,16 @@
+# Echo server program
+import socket
+
+HOST = ''                 # Symbolic name meaning all available interfaces. It will acept whoever is wanting to connect.
+PORT = 50007              # Arbitrary non-privileged port
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:	#Instantiates the socket and sets to "s" for use later
+    s.bind((HOST, PORT))	#Binds the host and port together. Basically makes this the address of the client
+    s.listen(1)		#Means that the server is only listening to one client. Can be multiple though.
+    conn, addr = s.accept()	#Sets the accepted socket connection to two variables for use later.
+    with conn:
+        print('Connected by', addr)	#Prints who connected with. Like the address. 
+        while True:	#States that while receiving information (data), it will store in data. 
+            data = conn.recv(1024)
+            if not data: break	#If no data is received from the client then it will break out of the loop
+            conn.sendall(data) #Will continue to receive data until no more is sent.
+            print(data) #This will print whatever was received from the client.
