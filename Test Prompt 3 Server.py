6dae2ca695cf53a80a4afcc47fe4c604c4ce2e27
lkmflam <53613483+lkmflam@users.noreply.github.com>
2019-12-20 11:26:11 -0500

    Test Prompt #3 Completed

--- Test Prompt 3 Server.py
+++ Test Prompt 3 Server.py
@@ -2,7 +2,7 @@
 import socket
 
 HOST = ''                 # Symbolic name meaning all available interfaces. It will acept whoever is wanting to connect.
-PORT = 50007              # Arbitrary non-privileged port
+PORT = 50111              # Arbitrary non-privileged port
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:	#Instantiates the socket and sets to "s" for use later
     s.bind((HOST, PORT))	#Binds the host and port together. Basically makes this the address of the client
     s.listen(1)		#Means that the server is only listening to one client. Can be multiple though.
@@ -11,10 +11,20 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:	#Instantiates the s
         print('Connected by', addr)	#Prints who connected with. Like the address. 
         while True:	#States that while receiving information (data), it will store in data. 
             data = conn.recv(1024)
+            data = data.decode()
             if not data: break	#If no data is received from the client then it will break out of the loop
             print(data) #This will print whatever was received from the client.
-            vowel = "a", "e", "i", "o", "u"
-            for vowel in data:
-                data.replace(vowel, "#")
-            print(data)
-            conn.sendall(data) #Will send back what it recieved.
+            if "a" in data:
+                data = data.replace("a", "#")
+            if "e" in data:
+                data = data.replace("e", "#")
+            if "i" in data:
+                data = data.replace("i", "#")
+            if "o" in data:
+                data = data.replace("o", "#")
+            if "u" in data:
+                data = data.replace("u", "#")
+            final = data
+            print(final)
+            finalstring = final.encode()
+            conn.sendall(finalstring) #Will send back what it recieved.
